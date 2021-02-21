457c4255fbda01029f4a2db7e43e26b67f1325b9
Pietro Gonçalves <pietrogon@gmail.com>
2021-02-21 18:51:09 -0300

    update

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -10,18 +10,30 @@ module.exports = app => {
   }
 
   users.getUser = (req, res) => {
-    var user = req.body;
-    res.status(200).json(db.run('select ' + id));
+    let params = req.params;
+    let sql = db.run('select * from users where pk_id = ' + params.userId)
+    console.log(sql);
+    res.status(200).json(sql);
   }
 
   users.deleteUser = (req, res) => {
-    var user = req.body;
-    res.status(200).json(db.run('delete ' + user.id));
+    let params = req.params;
+    let sql = db.run('delete from users where pk_id = ' + params.userId)
+    console.log(sql);
+    res.status(200).json(sql);
   }
 
   users.updateUser = (req, res) => {
+    let params = req.params;
+    let user = req.body;
+    let sql = db.run('update users set email = ' + user.email + ', name = ' + user.name + ', password = ' + user.password + ' where pk_id = ' + params.userId)
+    console.log(sql);
+    res.status(200).json(sql);
+  }
+
+  users.createUser = (req, res) => {
     var user = req.body;
-    res.status(200).json(db.run('update ' + user.id + user));
+    res.status(200).json(db.run("INSERT INTO users (name, email, password) VALUES('"+user.name+"', '" + user.email + "', '" + user.password + "');"));
   }
 
   return users;
--- api/routes/users.js
+++ api/routes/users.js
@@ -2,5 +2,11 @@ module.exports = app => {
   const controller = app.controllers.users;
 
   app.route('/api/users')
-    .get(controller.getListaUsers);
+    .get(controller.getListaUsers)
+    .push(controller.createUser);
+  
+  app.route('/api/users/:userId')
+    .get(controller.getUser)
+    .delete(controller.deleteUser)
+    .put(controller.updateUser)
 }
