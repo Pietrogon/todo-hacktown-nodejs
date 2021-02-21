bd8b1c3e804007c16b9bd5ae63cf4dd8a34a0fa5
victorbortolotto <victorcampobortolotto@gmail.com>
2021-02-21 18:54:03 -0300

    Update

--- config/createTables.js
+++ config/createTables.js
@@ -4,7 +4,7 @@ createTables = () => {
   db.connect('../database.db');
 	
 	console.log(db.run(`
-		CREATE TABLE IF NOT EXISTS usersId(
+		CREATE TABLE IF NOT EXISTS users(
 			pk_id integer primary key autoincrement,
 			email text NOT NULL UNIQUE,
 			name text NOT NULL,
@@ -14,24 +14,22 @@ createTables = () => {
 
 	console.log(db.run(`
 		CREATE TABLE IF NOT EXISTS categorys(
-			pk_category primary key autoincrement,
-			user integer,
-			category integer,
+			pk_id primary key autoincrement,
+			userId integer NOT NULL,
 			categoryName text NOT NULL		
 		);
   `));		
 
 	console.log(db.run(`
 		CREATE TABLE IF NOT EXISTS tasks(
+			pk_id integer primary key autoincrement,
 			title text NOT NULL, 
-			user integer,
-			category integer,
+			userId integer NOT NULL,
+			categoryId integer,
 			task text,
 			status text	
 		);
 	`));
-
-	//console.log(db.run("INSERT INTO users (name, email, password) VALUES('Bud Powell', 'batata', 'senha');"));
 
 	return db;
 }
