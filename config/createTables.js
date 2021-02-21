cd8d0eb6adc8ae3ccd0c77278b9bfb0cc56f1403
victorbortolotto <victorcampobortolotto@gmail.com>
2021-02-21 18:40:21 -0300

    Update
    Adiciona novas colunas as tabelas

--- config/createTables.js
+++ config/createTables.js
@@ -4,7 +4,7 @@ createTables = () => {
   db.connect('../database.db');
 	
 	console.log(db.run(`
-		CREATE TABLE IF NOT EXISTS users(
+		CREATE TABLE IF NOT EXISTS usersId(
 			pk_id integer primary key autoincrement,
 			email text NOT NULL UNIQUE,
 			name text NOT NULL,
@@ -14,22 +14,22 @@ createTables = () => {
 
 	console.log(db.run(`
 		CREATE TABLE IF NOT EXISTS categorys(
-
-			pk_category primary key autoincrement	
-
+			pk_category primary key autoincrement,
+			user integer,
+			category integer,
+			categoryName text NOT NULL		
 		);
   `));		
 
 	console.log(db.run(`
-		CREATE TABLE IF NOT EXISTS lists(
-			  
+		CREATE TABLE IF NOT EXISTS tasks(
 			title text NOT NULL, 
-			componentList text	
-			
+			user integer,
+			category integer,
+			task text,
+			status text	
 		);
 	`));
-	
-
 
 	//console.log(db.run("INSERT INTO users (name, email, password) VALUES('Bud Powell', 'batata', 'senha');"));
 
