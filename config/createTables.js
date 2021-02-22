c03a712a178bdbed23c2f475fbf97eebddd5226b
victorbortolotto <victorcampobortolotto@gmail.com>
2021-02-21 21:28:09 -0300

    Update
    Ajustado a organização das colunas e adicionado arquivos task a pasta controllers e routes

--- config/createTables.js
+++ config/createTables.js
@@ -16,16 +16,16 @@ createTables = () => {
 		CREATE TABLE IF NOT EXISTS categorys(
 			pk_id primary key autoincrement,
 			userId integer NOT NULL,
-			categoryName text NOT NULL		
+			name text NOT NULL		
 		);
   `));		
 
 	console.log(db.run(`
 		CREATE TABLE IF NOT EXISTS tasks(
 			pk_id integer primary key autoincrement,
-			title text NOT NULL, 
 			userId integer NOT NULL,
 			categoryId integer,
+			title text NOT NULL, 
 			task text,
 			status text	
 		);
